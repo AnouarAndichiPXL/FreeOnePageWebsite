f687d2585e018b09b14b43c5ebd4f9d95d4acf2a
AnouarAndichiPXL <73337199+AnouarAndichiPXL@users.noreply.github.com>
2021-08-25 14:18:40 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict'
+
+
+window.addEventListener('load', WindowHandel);
+
+
+function WindowHandel(){
+    // Slider function
+    sliderAutomatique();
+
+    //Btn to go Down 
+    document.getElementsByClassName('down-btn')[0].addEventListener('click', function(){
+        window.location = "#about"
+    });
+
+    let open = false;
+    //Hamburger icon fot mobile devices 
+    document.getElementsByClassName('hamburger')[0].addEventListener('click', function(){
+        if (open === false) {
+            for (let index = 0; index < document.getElementsByClassName('nav-item').length; index++) {
+                document.getElementsByClassName('nav-item')[index].style.display = "block";
+            }
+            open = true;
+            }else{
+                for (let index = 0; index < document.getElementsByClassName('nav-item').length; index++) {
+                    document.getElementsByClassName('nav-item')[index].style.display = "none";
+                }            
+                open = false;
+        }
+    });
+
+
+
+
+function sliderAutomatique(){
+    let slider = document.getElementsByClassName('slider')[0];
+    let sliderIndex = 1;
+
+    setInterval(function(){ 
+        if(sliderIndex > 3) {sliderIndex= 1}
+        slider.style.backgroundImage  = "linear-gradient(to bottom,rgba(0, 0, 0, 0.6) 0%, rgba(0, 0, 0, 0.6) 100%),url('../images/" + "slide_0" + sliderIndex +  ".jpg  ')";
+        sliderIndex++;
+    }, 4000);
+}
+}
